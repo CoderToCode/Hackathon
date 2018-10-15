019d6d6fda68b7ff614ff41c9bc09a565b56a05b
CoderToCode <sagarpatel3012000@gmail.com>
2018-10-15 17:10:44 +0530

    1

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi I am SAGAR</h1>
+    <p>I am from KiTE, perssuing First year in CSE</p>
+    <p>I am interested in coding</p>
+    <p>I like to get free ticket for the hackathon</p>
+        <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
